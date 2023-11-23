a7f368974347cc4a914ae6c33339ca6034154977
CaroPohn <carolinapohn@gmail.com>
2023-11-23 10:47:35 -0300

    Add rules update

--- src/Screens/Rules.cpp
+++ src/Screens/Rules.cpp
@@ -4,14 +4,30 @@ namespace flappybird
 {
 	namespace rules
 	{
+		Sprite BackMenuRulesButton;
+
 		void InitRules()
 		{
-
+			BackMenuRulesButton.texture = LoadTexture("res/credits/ReturnMenuButton.png");
+			BackMenuRulesButton.position = { static_cast<float>(GetScreenWidth() - BackMenuRulesButton.texture.width),static_cast<float>(GetScreenHeight() - BackMenuRulesButton.texture.height) };
+			BackMenuRulesButton.scale = 0.5f;
 		}
 
 		void RulesUpdate(Screen& currentScene)
 		{
+			if (MouseColision(BackMenuRulesButton))
+			{
+				BackMenuRulesButton.color = GRAY;
 
+				if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+				{
+					currentScene = Screen::Menu;
+				}
+			}
+			else
+			{
+				BackMenuRulesButton.color = WHITE;
+			}
 		}
 
 		void RulesDrawing()
