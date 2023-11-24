c697f1245be683bbc58b3d3acec9d64858b21ec0
CaroPohn <carolinapohn@gmail.com>
2023-11-24 11:51:48 -0300

    Add pause moving functions from lose screen and integrate them

--- src/Screens/Pause.cpp
+++ src/Screens/Pause.cpp
@@ -1,22 +1,73 @@
 #include "Pause.h"
 
+#include <iostream>
+#include <string>
+
+#include "ProgramUtilities/Utilities.h"
+
 namespace flappybird
 {
 	namespace pause
 	{
+		static Sprite BackMenuButton;
+		static Sprite PlayAgain;
+
 		void InitPause()
 		{
+			BackMenuButton.position = { 400,550 };
+			BackMenuButton.scale = 0.6f;
+			BackMenuButton.texture = LoadTexture("res/credits/ReturnMenuButton.png");
 
+			PlayAgain.position = { 200,650 };
+			PlayAgain.scale = 0.6f;
+			PlayAgain.texture = LoadTexture("res/menu/OnePlayer.png");
 		}
 
-		void PauseUpdate(bool& isGameOver)
+		void PauseUpdate(Screen& currentScene, bool& isPaused)
 		{
+			if (MouseColision(BackMenuButton))
+			{
+				BackMenuButton.color = GRAY;
 
+				if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+				{
+					currentScene = Screen::Menu;
+				}
+			}
+			else
+			{
+				BackMenuButton.color = WHITE;
+			}
+
+			if (MouseColision(PlayAgain))
+			{
+				PlayAgain.color = GRAY;
+
+				if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT))
+				{
+					isPaused = false;
+				}
+			}
+			else
+			{
+				PlayAgain.color = WHITE;
+			}
 		}
 
-		void DrawPause()
+		void DrawPause(bool isGameOver, int pointsCounter)
 		{
-
+			if (isGameOver)
+			{
+				DrawText(std::to_string(pointsCounter).c_str(), GetScreenWidth() / 2, GetScreenHeight() / 2, 70, WHITE);
+				DrawSprite(BackMenuButton);
+				DrawSprite(PlayAgain);
+			}
+			else
+			{
+				DrawText("PAUSED", GetScreenWidth() / 2, GetScreenHeight() / 2, 70, WHITE);
+				DrawSprite(BackMenuButton);
+				DrawSprite(PlayAgain);
+			}
 		}
 	}
 }
--- src/Screens/Pause.h
+++ src/Screens/Pause.h
@@ -1,12 +1,16 @@
 #pragma once
 
+#include "ProgramUtilities/Utilities.h"
+
+#include "ScreenManagements/Screens.h"
+
 namespace flappybird
 {
 	namespace pause
 	{
 		void InitPause();
-		void PauseUpdate(bool& isGameOver);
-		void DrawPause();
+		void PauseUpdate(Screen& currentScene, bool& isPaused);
+		void DrawPause(bool isGameOver, int pointsCounter);
 	}
 
 }
